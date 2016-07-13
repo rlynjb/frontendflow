a96a9e8acb1098288afff639e38482b0e9395a1c
Reina Lyn Ben <reinalynben@C02RQ0GCG8WP.local>
2016-07-13 09:54:40 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-gulp.task('sass', function() {
+/*
+  compile
+  sass - vendors - vendor.css
+  sass - custom - custom.css
+
+  js - vendors - vendor.js
+  js - custom - custom.js
+
+  watch sass
+  watch js
+*/
+
+gulp.task('vendorStyles', function() {
   return gulp.src([
-    './css/styles.scss'
-  ]) // source files
+    './bower_components/font-awesome/scss/font-awesome.scss',
+    './css/foundation-includes.scss'
+  ])
   .pipe(sass({
-    includePaths: [ './bower_components/foundation-sites/scss/' ],
-    outputStyle: 'nested',
-    errLogToConsole: true
-  })) // using gulp-sass
-  .pipe(gulp.dest('./css')) // destination
+    includePaths: [
+      './bower_components/font-awesome/scss',
+      './bower_components/foundation-sites/scss'
+    ],
+    outputStyle: 'compressed'
+  }))
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('./css'));
 });
+
+gulp.task('customStyles', function() {
+  return gulp.src([
+    './css/custom.scss'
+  ])
+  .pipe(sass())
+  .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./css/*.scss', ['vendorStyles', 'customStyles']);
+});
+
+gulp.task('default', ['vendorStyles', 'customStyles']);
