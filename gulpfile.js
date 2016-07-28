03b7f10997cc42d7ac9edd47fade89e4d27a9a26
Reina Lyn Ben <reinalynben@C02RQ0GCG8WP.local>
2016-07-28 13:06:15 -0700

    configure react to use jsx and coded build process on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,75 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var strip = require('gulp-strip-comments');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    strip = require('gulp-strip-comments');
 
-/*
-  compile
-  sass - vendors - vendor.css
-  sass - custom - custom.css
+// Source Files
+var sassFiles = [
+  './bower_components/font-awesome/scss/font-awesome.scss',
+  './css/foundation-includes.scss'
+]
+var sassSource = [
+  './bower_components/font-awesome/scss',
+  './bower_components/foundation-sites/scss'
+]
+var jsScripts = [
+  /* DOM */
+  './bower_components/react/react.js',
+  './bower_components/react/react-dom.js',
+  //'./bower_components/jquery/dist/jquery.js',
 
-  js - vendors - vendor.js
-  js - custom - custom.js
+  /* Animation */
+  //./bower_components/smoothstate/src/jquery.smoothState.js',
 
-  watch sass
-  watch js
-*/
+  /* Zurb Foundation stuff */
+  //'./bower_components/foundation-sites/js/foundation.core.js',
+  //'./bower_components/foundation-sites/js/foundation.util.box.js',
+  //'./bower_components/foundation-sites/js/foundation.util.keyboard.js',
+  //'./bower_components/foundation-sites/js/foundation.util.mediaQuery.js',
+  //'./bower_components/foundation-sites/js/foundation.util.motion.js',
+  //'./bower_components/foundation-sites/js/foundation.util.nest.js',
+  //'./bower_components/foundation-sites/js/foundation.util.timerAndImageLoader.js',
+  //'./bower_components/foundation-sites/js/foundation.util.touch.js',
+  //'./bower_components/foundation-sites/js/foundation.util.triggers.js',
+  //'./bower_components/foundation-sites/js/foundation.abide.js',
+  //'./bower_components/foundation-sites/js/foundation.accordion.js',
+  //'./bower_components/foundation-sites/js/foundation.accordionMenu.js',
+  //'./bower_components/foundation-sites/js/foundation.drilldown.js',
+  //'./bower_components/foundation-sites/js/foundation.dropdown.js',
+  //'./bower_components/foundation-sites/js/foundation.dropdownMenu.js',
+  //'./bower_components/foundation-sites/js/foundation.equalizer.js',
+  //'./bower_components/foundation-sites/js/foundation.interchange.js',
+  //'./bower_components/foundation-sites/js/foundation.magellan.js',
+  //'./bower_components/foundation-sites/js/foundation.offcanvas.js',
+  //'./bower_components/foundation-sites/js/foundation.orbit.js',
+  //'./bower_components/foundation-sites/js/foundation.responsiveMenu.js',
+  //'./bower_components/foundation-sites/js/foundation.responsiveToggle.js',
+  //'./bower_components/foundation-sites/js/foundation.reveal.js',
+  //'./bower_components/foundation-sites/js/foundation.slider.js',
+  //'./bower_components/foundation-sites/js/foundation.sticky.js',
+  //'./bower_components/foundation-sites/js/foundation.tabs.js',
+  //'./bower_components/foundation-sites/js/foundation.toggler.js',
+  //'./bower_components/foundation-sites/js/foundation.tooltip.js',
 
+  /* Your javascript file */
+  './js/custom.js'
+]
+var fontIcons = [
+  './bower_components/font-awesome/fonts/FontAwesome.otf',
+  './bower_components/font-awesome/fonts/fontawesome-webfont.eot',
+  './bower_components/font-awesome/fonts/fontawesome-webfont.svg',
+  './bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
+  './bower_components/font-awesome/fonts/fontawesome-webfont.woff',
+  './bower_components/font-awesome/fonts/fontawesome-webfont.woff2'
+]
+
+
+// Build Process
 gulp.task('styles', function() {
-  return gulp.src([
-    //'./bower_components/font-awesome/scss/font-awesome.scss',
-    './css/foundation-includes.scss'
-  ])
+  return gulp.src(sassFiles)
   .pipe(sass({
-    includePaths: [
-      //'./bower_components/font-awesome/scss',
-      './bower_components/foundation-sites/scss'
-    ],
+    includePaths: sassSource,
     outputStyle: 'compressed'
   }))
   .pipe(concat('styles.css'))
@@ -34,53 +77,22 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-  return gulp.src([
-    './bower_components/jquery/dist/jquery.js',
-    './bower_components/smoothstate/src/jquery.smoothState.js',
-    './bower_components/foundation-sites/js/foundation.core.js',
-    //'./bower_components/foundation-sites/js/foundation.util.box.js',
-    //'./bower_components/foundation-sites/js/foundation.util.keyboard.js',
-    './bower_components/foundation-sites/js/foundation.util.mediaQuery.js',
-    //'./bower_components/foundation-sites/js/foundation.util.motion.js',
-    //'./bower_components/foundation-sites/js/foundation.util.nest.js',
-    //'./bower_components/foundation-sites/js/foundation.util.timerAndImageLoader.js',
-    //'./bower_components/foundation-sites/js/foundation.util.touch.js',
-    //'./bower_components/foundation-sites/js/foundation.util.triggers.js',
-    //'./bower_components/foundation-sites/js/foundation.abide.js',
-    //'./bower_components/foundation-sites/js/foundation.accordion.js',
-    //'./bower_components/foundation-sites/js/foundation.accordionMenu.js',
-    //'./bower_components/foundation-sites/js/foundation.drilldown.js',
-    //'./bower_components/foundation-sites/js/foundation.dropdown.js',
-    //'./bower_components/foundation-sites/js/foundation.dropdownMenu.js',
-    //'./bower_components/foundation-sites/js/foundation.equalizer.js',
-    //'./bower_components/foundation-sites/js/foundation.interchange.js',
-    //'./bower_components/foundation-sites/js/foundation.magellan.js',
-    //'./bower_components/foundation-sites/js/foundation.offcanvas.js',
-    //'./bower_components/foundation-sites/js/foundation.orbit.js',
-    //'./bower_components/foundation-sites/js/foundation.responsiveMenu.js',
-    //'./bower_components/foundation-sites/js/foundation.responsiveToggle.js',
-    //'./bower_components/foundation-sites/js/foundation.reveal.js',
-    //'./bower_components/foundation-sites/js/foundation.slider.js',
-    //'./bower_components/foundation-sites/js/foundation.sticky.js',
-    //'./bower_components/foundation-sites/js/foundation.tabs.js',
-    //'./bower_components/foundation-sites/js/foundation.toggler.js',
-    //'./bower_components/foundation-sites/js/foundation.tooltip.js',
-    './js/custom.js'
-  ])
+  return gulp.src(jsScripts)
   .pipe(concat('scripts.js'))
   .pipe(strip())
   .pipe(gulp.dest('./js'));
 });
 
+gulp.task('app', function() {
+  return gulp.src('./js/app.jsx')
+  .pipe(babel({
+    presets: ['react', 'es2015']
+  }))
+  .pipe(gulp.dest('./js'));
+});
+
 gulp.task('fonts', function() {
-  return gulp.src([
-    './bower_components/font-awesome/fonts/FontAwesome.otf',
-    './bower_components/font-awesome/fonts/fontawesome-webfont.eot',
-    './bower_components/font-awesome/fonts/fontawesome-webfont.svg',
-    './bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
-    './bower_components/font-awesome/fonts/fontawesome-webfont.woff',
-    './bower_components/font-awesome/fonts/fontawesome-webfont.woff2'
-  ])
+  return gulp.src(fontIcons)
   .pipe(gulp.dest('./fonts'));
 });
 
@@ -88,4 +100,4 @@ gulp.task('watch', function() {
   gulp.watch(['./css/*.scss', './js/custom.js'], ['styles', 'scripts']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'fonts']);
+gulp.task('default', ['styles', 'scripts', 'app', 'fonts']);
