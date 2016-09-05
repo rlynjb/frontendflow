9bf30215fe5cfd813536d47f8c9390859a30c7b6
Reina Lyn Ben <reinalynben@C02RQ0GCG8WP.local>
2016-09-05 16:11:17 -0700

    add js scroll page

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 $(function() {
   $(document).foundation();
+
+  /*
+    Anchorless Page Scroll
+    ref: http://jsfiddle.net/BjpWB/4/
+    HTML syntax: <a name="id5"/>
+  */
+  function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  }
+  $("#link").click(function() {
+    scrollToAnchor('id3');
+  });
 });
