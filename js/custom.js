de34db2ec8ac4f6529d93ca04de55776094e9bf6
Reina Lyn Ben <reinalynben@C02RQ0GCG8WP.local>
2016-12-02 10:05:29 -0800

    js updates

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,39 @@
 $(function() {
   $(document).foundation();
 
+
+  /*
+    AJAX usage:
+    - send data and pull data to display
+    - php form inteferance
+  */
+  /*
+    Contact Form ajax
+  */
+  $('#contact-form')
+  .on('submit', function(e) {
+    e.preventDefault();
+  })
+  .on('formvalid.zf.abide', function(e, frm) {
+    var theForm = $(this);
+
+    // Insert product registation data to database
+    // and send a copy to email
+    $.ajax({
+      data: theForm.serialize(),
+      type: 'POST',
+      url: 'insertcontact.php',
+      success: function(a, b, c) {
+        $('.form-wrapper').hide();
+        $('.thank-you-message').show();
+      },
+      error: function(a,b,c) {
+        $('.form-wrapper').hide();
+        $('.error-message').show();
+      }
+    });
+  });// end - on click
+
   /*
     Anchorless Page Scroll
     ref: http://jsfiddle.net/BjpWB/4/
@@ -13,4 +46,51 @@ $(function() {
   $("#link").click(function() {
     scrollToAnchor('id3');
   });
+
+
+  /*
+    Stops vimeo video when modal is closed
+  */
+  var videoModal = $('#videoModal iframe');
+  var videoSrc = videoModal.attr('src');
+
+  $('#videoModal .close-button').click(function() {
+    videoModal.attr('src', '');
+    videoModal.attr('src', videoSrc);
+  });
+
+
+  /*
+    Use window resize for realtime viewport detection
+    Disable detail modal on medium and up window screen
+  */
+  $(window).resize(function() {
+    if( $(this).width() > 639 && $('#detailModal[aria-hidden="false"]') ) {
+      $('#detailModal').foundation('close');
+    }
+  });
+
+
+  /*
+    checkbox group
+    ref: http://foundation.zurb.com/forum/posts/1967-abide-validation-on-checkbox-doesnt-group
+    ref: http://stackoverflow.com/questions/26890023/display-error-if-no-checkbox-is-checked-in-checkbox-group
+  */
+  Foundation.Abide.defaults.validators['checkbox_limit'] = function($el, required, parent) {
+    var group = parent.closest('.checkbox-group'),
+        max = group.attr('data-checkbox-validator-max'),
+        checked = group.find(':checked').length;
+
+    if (checked <= max && checked != 0) {
+      group.find('legend').css({
+        color: '#676767'
+      });
+      return true;
+    } else {
+      group.find('legend').css({
+        color: '#ef4d4d'
+      });
+      return false;
+    }
+  }
 });
